d700ac0fc1730f74e0e287d2ebc123c5f9c646a6
EugeniuChirilovici <chirilovici.janea@gmail.com>
2019-01-18 12:42:43 +0200

    add web config

---
+++ src/main/java/utm/config/WebConfig.java
@@ -0,0 +1,15 @@
+package utm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/resources/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/resources/css/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/resources/images/");
+    }
+}
